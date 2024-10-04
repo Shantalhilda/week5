32f6029e3daaed774737cf74b0aefafb56fe109d
shantal hilda <shanhildah2@gmail.com>
2024-10-04 11:19:36 +0300

    Add files via upload

---
+++ asignment2.js
@@ -0,0 +1,13 @@
+//function
+function calculate_Profit(cost_price,selling_price,unit_price){
+    var cost_price = 100;
+    var selling_price = 120;
+    var unit_price = 10;
+    var profit = selling_price - cost_price;
+    console.log("Profit:", profit);
+    return profit;
+
+    
+}
+calculate_Profit();
+
