1a0049c75924680e94262311f468aaadfcc0cb05
shantal hilda <shanhildah2@gmail.com>
2024-10-06 11:52:20 +0300

    Add files via upload

---
+++ push10functions.js
@@ -0,0 +1,11 @@
+//functons with parameters
+sum_three = 44;
+
+function summation_three (num_five,num_six){
+    num_five = 10;
+    num_six = 5;
+    var sum_four = num_five + num_six;
+    console.log(sum_three + sum_four);
+}
+
+summation_three(44);
