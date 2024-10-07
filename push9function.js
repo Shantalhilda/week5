babf89d2aca54fa8980a54cd0d7293f213d06b57
shantal hilda <shanhildah2@gmail.com>
2024-10-07 11:36:33 +0300

    Add files via upload

---
+++ push9function.js
@@ -0,0 +1,18 @@
+//Returning function
+
+function summation () {
+    
+    var num_five = 10;
+    var num_six = 15;
+    //addition
+    var sum_two = num_five + num_six;
+    //printing sum
+    console.log(sum_two);
+
+    return sum_two;
+}
+
+
+//assigning a value to a variable
+
+var sum_two= summation();
