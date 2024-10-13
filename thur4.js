6a66ef8dabf6a23e542c9255e177108d266a8b97
shantal hilda <shanhildah2@gmail.com>
2024-10-13 20:04:01 +0300

    Add files via upload

---
+++ thur4.js
@@ -0,0 +1,8 @@
+let temperature = 30;
+if (temperature > 25) {
+    console.log("It's hot!");
+}else if (temperature >15 && temperature <25){
+    console.log("It's moderate!");
+}else{
+    console.log("its cold!")
+}
