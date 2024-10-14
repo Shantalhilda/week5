fd1642950ede20a62017019b3c9d94f2528d2f05
shantal hilda <shanhildah2@gmail.com>
2024-10-14 17:28:06 +0300

    Add files via upload

---
+++ thur5.js
@@ -0,0 +1,10 @@
+var email = "shanhildah2@gmail.com";
+var phone = "0705163344";
+var password = "1234"
+
+if(email == "shanhildah2@gmail.com" || phone == "0705163344" && password == "1234"){
+    console.log("Login Successful!");
+}else{
+    console.log("Invalid credentials. Please try again.");
+}
+
