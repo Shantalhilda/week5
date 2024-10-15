97cffc3bb783bd5caddfb4b7acfc7b18cf3df632
shantal hilda <shanhildah2@gmail.com>
2024-10-15 21:35:19 +0300

    Add files via upload

---
+++ thur6.js
@@ -0,0 +1,11 @@
+
+
+
+let age = 18;
+if (age >= 65) {
+    console.log("you are of retirement age.");
+}else if(age < 65 && age >=18) {
+    console.log("You are eligible to work.")
+}else{
+    console.log("you are too young to work.")
+} 
