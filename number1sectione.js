7fa65f80c532689228407ba03f14155f7b221320
shantal hilda <shanhildah2@gmail.com>
2024-10-21 14:29:58 +0300

    Add files via upload

---
+++ number1sectione.js
@@ -0,0 +1,6 @@
+// Rainfall data in millimeters for the last 10 
+let rainfallData = [10, 12, 8, 5, 6, 14, 7, 3, 11, 9];
+
+for (let i = 0; i < rainfallData.length; i++) {
+    console.log(`Day ${i + 1}: ${rainfallData[i]} mm`);
+}
