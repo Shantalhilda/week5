fe767cacb305c38a692769f9066c077a310ba8ad
shantal hilda <shanhildah2@gmail.com>
2024-10-24 23:02:00 +0300

    Add files via upload

---
+++ number3sectionA.js
@@ -0,0 +1,9 @@
+//difference between var let and const
+
+//when reassigning values to var , they can be easily changed.
+
+//when reassigning values to let, the figures can be changed easily to what you 
+  //want then to be
+
+//when assigning values to const, the values cannot be easily changed since 
+  //you have assigned a constant to it 
