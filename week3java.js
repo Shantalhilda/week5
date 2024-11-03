9cbb7a5c3b8882ec90bcc7a167152fb2a586403c
shantal hilda <shanhildah2@gmail.com>
2024-11-03 23:00:13 +0300

    Add files via upload

---
+++ week3java.js
@@ -0,0 +1,18 @@
+//printing in java
+console.log("hello shantal");
+
+
+let first_name = "shan";
+let last_name = "dan";
+
+const PIE = 3.13423;
+const MAX_SPEED = 5000;
+
+//variables
+var school_name = "UCU";
+
+//ONE LINE COMENET
+
+/*
+MULTIPLE LINE COMMENT
+*/
