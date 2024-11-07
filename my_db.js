7d46d4a980377335d0cbfeb4c8a4412dea6d8d40
shantal hilda <shanhildah2@gmail.com>
2024-11-07 18:43:27 +0300

    Add files via upload

---
+++ my_db.js
@@ -0,0 +1,7 @@
+let friends = [
+    {"id":1,"name":"tom"},
+    {"id":2,"name":"peter"},
+    {"id":3,"name":"jane"},
+    {"id":4,"name":"james"},
+    {"id":5,"name":"shan"}];
+module.exports = friends;
