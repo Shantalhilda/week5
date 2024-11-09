49a28ae035add6eca968b941130c5d570bb41c74
shantal hilda <shanhildah2@gmail.com>
2024-11-09 23:22:25 +0300

    Add files via upload

---
+++ coursework.py
@@ -0,0 +1,9 @@
+names = ["linda", "agnes", "sam"]
+"""for i in names:
+    print(i)"""
+"""print(names[0])"""
+"""print(names[1])"""
+"""print(names[-1])"""
+#replace a name without using a function
+names[0] = "shantal"
+print(names)
