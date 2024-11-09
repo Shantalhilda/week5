28b5ecd54ec738705d83e7b7ad394b2d534c40f5
shantal hilda <shanhildah2@gmail.com>
2024-11-09 23:20:25 +0300

    Add files via upload

---
+++ wambete.py
@@ -0,0 +1,29 @@
+# creating variables
+"""name = "shantal"
+age = 20
+colour = "brown"
+book_price = 2500
+
+
+#print variable values abd types
+name = "shantal"
+age = 20
+colour = "black"
+book_price = 2500
+print("name:",name, "Type:", type(name) )
+print("age:", age, "Type:", type(age))
+print("colour:", colour,"Type:", type(colour))
+print("book_price:", book_price, "Type:", type(book_price))"""
+
+# perform operations
+age = 20 +5
+book_price = 2500 * 3 
+str1 = "shantal"
+str2 = "world"
+print("string 1:",str1)
+print("string 2:",str2)
+print("concertenated two different string:",str)
+print(age)
+print(book_price)
+
+  
