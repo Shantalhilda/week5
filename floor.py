d148d697172e6068c3fcedf1359f6bb4289913c5
shantal hilda <shanhildah2@gmail.com>
2024-11-09 23:22:55 +0300

    Add files via upload

---
+++ floor.py
@@ -0,0 +1,32 @@
+#floor division
+"""print(7/2)
+print(7//2)
+print(-7//2)"""
+
+
+#
+"""print("welcome")
+for x in range(3):
+    print(x)
+print("goodbye")"""
+
+
+"""x = pow(1,5)
+print(x)"""
+
+
+"""def calculate_area(length,width):
+    area = length * width
+    return area
+rectangle_area = calculate_area(10,5)
+print(f'the area of the rectangle is{rectangle_area}')"""
+
+
+def calculate_sum_age(Age_1,Age_2):
+ sum_age = sum(Age_1,Age_2)
+ return sum_age
+total_age = calculate_sum_age(20,19) 
+print(f"the sum of the age is {total_age}")
+
+
+
