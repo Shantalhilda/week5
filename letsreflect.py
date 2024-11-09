41dad05c1798ee6d1fb35666382ed7ace9f859bd
shantal hilda <shanhildah2@gmail.com>
2024-11-09 23:24:32 +0300

    Add files via upload

---
+++ letsreflect.py
@@ -0,0 +1,25 @@
+#number1
+#finding average score
+"""exam_score = [20,70,30,90,40]
+#finding the total number of items in the list
+total_items =len(exam_score)
+#finding  the sum of the items in the list
+total = sum(exam_score) 
+#finding the average
+average = total / total_items
+#printing the average
+print(f"average score is: {average}")"""
+
+
+
+#removing duplicates
+mylist = ["a", "b", "a", "d", "b"]
+mylist = mylist(dict.fromkeys(mylist))
+print(mylist)
+
+
+
+"""name = "shantal"
+print(type(name))"""
+
+
