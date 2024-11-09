792047b697cae35686b8b3ba7de36d729c1c12a4
shantal hilda <shanhildah2@gmail.com>
2024-11-09 23:24:15 +0300

    Add files via upload

---
+++ functons&calling.py
@@ -0,0 +1,73 @@
+# functions and calling them out
+"""def hello():
+    print("hi there")
+    print("im a function")
+hello()
+
+
+
+def hello():
+    print("good morning shantal")
+    print("hope you are good!!")
+    print("its hillary")    
+hello()"""
+
+
+
+def main():
+    print("i have a message for you!")
+    message()
+print("goodbye")  
+def message():
+    print("the password is 'foo'")  
+    
+main()
+
+
+
+
+
+#average
+"""def average(num1,num2,num3):
+    print(f"{num1} , {num2} , {num3}")
+    sum = num1 + num2 + num3
+    average = sum/3
+    print(average)
+average(100,90,92)"""
+
+
+#area
+"""def calculateAreaofrectangle():
+    length = 30
+    width = 15
+    area = length * width
+    print(f"area is {area}")
+
+calculateAreaofrectangle()"""
+
+
+#function to call your names
+"""def names():
+    user_input = input("enter your names")
+    print(f"your names are {user_input}")
+names()"""
+
+#return name
+"""def name():
+    name = input(f"enter your names")
+    
+    return name
+print("your name is",name())"""
+
+#parameters
+"""def calculateareaofrectangleparameter(length,width):
+    area = length*width
+    print(f"Area is {area}")
+calculateareaofrectangleparameter(10,5)
+calculateareaofrectangleparameter(20,10)"""
+
+
+
+
+    
+    
