f1ee887b954cb9e54649c44739650eecd88f1e2f
shantal hilda <shanhildah2@gmail.com>
2024-11-10 21:16:47 +0300

    Add files via upload

---
+++ testpreparation.py
@@ -0,0 +1,73 @@
+#multplication  table
+"""def multiplication_table():
+    number = int(input("enter number: "))
+    for i in range(1,number+1):
+     print(f"{number} * {i} = {number * i}")
+multiplication_table ()"""
+
+#grading
+"""std_marks = int(input("enter students marks: "))
+if std_marks >= 95:
+    print(f"you have A") 
+elif std_marks >=80:
+    print(f"you have B")
+elif std_marks >= 70:
+    print("you have C")
+elif std_marks >= 60:
+    print(f"you have a D")
+elif std_marks >=55:
+    print(f"you have E")
+else:
+    print(f"you have F")"""
+    
+#palindrome checker
+# Ask the user to input a string
+#input_string = input("Enter a string: ")
+# Remove spaces and convert to lowercase for case-insensitive comparison
+#processed_string = input_string.replace(" ", "").lower()
+# Check if the processed string is equal to its reverse
+#if processed_string == processed_string[::-1]:
+ #   print(f"'{input_string}' is a palindrome.")
+#else:
+ #   print(f"'{input_string}' is not a palindrome.")
+
+
+#number 7
+#name = "shantal"
+#reverse = name[::-1]
+#print(reverse)
+
+
+#number 8(substring finder)
+#def is_substring(str1, str2):
+    #str1 = input("enter the first string: ")
+    #str2 = input("enter the second string: ")
+    #if str2 in str1:
+   #      return True 
+  #  else:
+ #       return False
+#is_substring(str1,str2)
+
+
+#number 6(word counter)
+#sentence = len("spider man away from home is out" .split())
+#print(f"the number of words in a sentence are {sentence}")
+
+
+#l = [1,2,3,4]
+#print(len(l))
+
+#summation of all even numbers
+user_defined_upper_limit = 50
+for i in range(1,50):
+    if  i % 2 == 0 :
+        print(i)
+    add = sum (i)
+    print(add)
+    i = i + 1 
+    
+
+  
+
+
+        
