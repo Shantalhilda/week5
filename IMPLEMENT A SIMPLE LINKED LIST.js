8206460448e24aa56fc52a3a1c6b68ed08c8c82f
shantal hilda <shanhildah2@gmail.com>
2024-11-18 23:08:53 +0300

    Add files via upload

---
+++ IMPLEMENT A SIMPLE LINKED LIST.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    add(data) {
+        let newNode = new Node(data);
+        if (!
