d93f50a732be7740b6285029955224cf4927fe74
shantal hilda <shanhildah2@gmail.com>
2024-11-22 23:58:54 +0300

    Add files via upload

---
+++ LONGEST COMMON SUBSEQUENCE.js
@@ -0,0 +1,19 @@
+function longestCommonSubsequence(str1, str2) {
+    let m = str1.length;
+    let n = str2.length;
+    let dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+console.log(longestCommonSubsequence("abcde", "ace")); // Output: 3 ("ace")
