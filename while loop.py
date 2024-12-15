5c04d77a4868b17c98f2173b8b876c89fb8da4a0
shantal hilda <shanhildah2@gmail.com>
2024-12-15 13:51:06 +0300

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,43 @@
+#whether
+""" condition = 1
+
+while condition < 10:
+    print("Nabulo")
+    condition = condition + 1
+    
+#another code
+number_1 = 6
+print(number_1)
+print("shantal")
+number_2 = 7
+print(number_2)
+print("shantal")
+number_3 = 8
+print(number_3)
+print("shantal")
+number_4 = 9 
+print(number_4)
+number_5 = 10
+print("shantal")
+number_6= 11
+print(Day-67)
+
+
+condition = 90
+
+while condition >= 1:
+    condition = condition - 1
+    
+    print((f"days to independence  {condition}"))"""
+    
+#even number
+condition = 999
+while condition <=200:
+    if condition % 2 == 0:
+        print(condition)
+        condition = condition + 1 
+        
+        
+        
+    
+    
