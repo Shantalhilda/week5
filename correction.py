281be9d08965be3a9a49a1c1501d7724489ab21d
shantal hilda <shanhildah2@gmail.com>
2024-12-16 20:16:40 +0300

    Add files via upload

---
+++ correction.py
@@ -0,0 +1,6 @@
+total_votes = int(input('Total votes: '))
+candidate_1 = int(input("enter the number of votes of candidate 1: "))
+candidate_2 = int(input("enter the number of votes of candidate 2: "))
+percentage_candidate_1 = candidate_1 * 100 / total_votes 
+percentage_candidate_2 = candidate_2 * 100 / total_votes  
+print(f"the percentage of candidate 1 is {percentage_candidate_1} and the percentage of candidate 2 is {percentage_candidate_2}) ")
