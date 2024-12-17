a6a673c7516832ade42e304806af17e1f42d038b
shantal hilda <shanhildah2@gmail.com>
2024-12-17 21:16:09 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,39 @@
+# 
+"""num_1 = input(first_number)
+num_2 = input(int("second_number"))
+sum = first_number + second_number
+print(f"sum")
+
+
+
+
+print("hurreeey")
+print("now, select 1 for addition, 2 for subtraction, 3 for division, 4 for multiplication")
+print("1, +")
+print("2, -")
+print("4, /")
+print("3, *")
+
+
+# storing the operator
+operate = int(input())
+print("woop woop")
+print("please enter the second number")
+second_number = int(input())
+
+computation =  0
+if operator == 1:
+    print("what is",first_number,"+",second_number)
+    right_answer = first_number+second_number
+    
+if operator == 2:
+    print("what is",first_number,"-",second_number)
+    right_answer = first_number-second_number
+    
+if operator ==3:
+    print("what is",first_number,"*",second_number)
+    right_answer = first_number*second_number
+    
+if operator ==4:
+    print("what is",first_number,"/",second_number)
+    right_answer = first_number/second_number"""
