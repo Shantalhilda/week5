3581823589ed4fc62418997ea194c9c2c807b928
shantal hilda <shanhildah2@gmail.com>
2024-12-19 20:58:14 +0300

    Add files via upload

---
+++ practising.py
@@ -0,0 +1,51 @@
+"""name = "shantal"
+print(name)
+
+name = input("what is your name")
+print(f"my name is {name}")
+
+YOB = int(input("enter your YOB"))
+age = 2024 - YOB
+print(f"you are {age} years old") 
+
+age = int(input("enter your age"))
+if age >= 20:
+    print("your od enough to vote")
+else:
+    print("you are not allowed to vote")
+    
+    
+length_rectangle = float(input("enter the length of rectangle"))
+width_rectangle = float(input("enter the width of the rectangle"))
+area_rectangle = length_rectangle * width_rectangle
+print(f"the area of the rectangle is {area_rectangle}")
+
+
+name = "shantal"
+print(type(name))
+
+age = 20
+print(type(age))
+
+book_price = 2500
+print(type(book_price))
+
+area = 2.5
+print(type(area))
+
+attendance = True
+print(type(attendance)) 
+
+
+
+
+
+name = int(input("enter your name")) 
+print("hello", (name))"""
+          
+
+
+a = 5
+b = 2.5
+sum = a+b
+print(float(a+b))
