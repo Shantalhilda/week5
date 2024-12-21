f073b4029afd8e3e2541b2de1f9ab084315d1399
shantal hilda <shanhildah2@gmail.com>
2024-12-21 18:56:09 +0300

    Add files via upload

---
+++ lists & tuples.py
@@ -0,0 +1,197 @@
+#lists
+"""this_list = ["apple","banana","passion","mango"]
+print(f"{this_list}") 
+
+
+#number of things in your list
+this_list = ["apple","banana","strawberry"]
+print(len(this_list))
+
+#data types of each list
+list_1 = ["apple","banana"]
+list_2 = [True,False,True]
+list_3 = [1,2,3,0]
+print(type(list_2))
+
+#duplicate lst
+this_list = ["apple","cherry","banana","apple","cherry","banana","apple"]
+print(f"{this_list}")
+
+#tuples
+this_tuple = ("apple","strawberry","cherry")
+print(f"{this_tuple}")
+
+#data types of tuples
+tuple_1 = ("apple","mango")
+tuple_2 = (True,False,True)
+tuple_3 = (1,2,3)
+print(type(tuple_1))
+print(type(tuple_2))
+print(type(tuple_3))
+
+
+
+list = ["a", "b"]
+for i in list:
+    print(i)
+    
+    
+list = ["d", "o"]
+for  i in range(len(list)):
+    print(i)
+    
+    
+for i in range(4) #0,1,2,3
+    print(i)
+    
+    
+    
+    
+list = ["apple","banana"]
+print(len(list))
+
+
+list = ["a","b","c","d","e"]
+for item in range(4):
+    print('foo')
+    print(item)
+    
+    
+list = ["apple","orange"]
+for i in list:
+    print (i)"""
+    
+ 
+    
+#remove list
+'''list= [1,2,3,4,5,6,7,8,9,0]
+list.remove(3)
+
+print(list)'''
+
+
+
+#adding something in a list
+
+
+#
+
+
+
+
+
+# display all elents in the list using while loop
+"""class_names = ["juniour", "mary", "gerald", "john", "shantal"]
+condition = 0
+while condition <len(class_names) :
+    print(class_names[condition])     
+    condition = condition + 1
+    
+#create a lst having districts
+district_list = ["jinja", "mukono", "kampala", "wakiso", "gulu", "arua", "masaka", "nebbbi", "karamoja","mbale"]
+district_list.append("kabale",)
+district_list.append("kibale")
+district_list.append("kasese")
+
+print(district_list)
+
+#list size
+print(len(district_list))
+
+#display all elements using for loop
+for districts in district_list:
+    print(districts) 
+    
+    
+#remove district
+district_list.remove("index")
+
+print(district_list)
+
+
+#while
+condition = 0
+while condition < len(district_list):
+    print(district_list[condition])
+    condition = condition + 1 """
+    
+    
+    
+#add an element to a list
+#l=[8,9,9]
+"""l.append(6)
+
+print(l)"""
+#print(l(2))
+
+
+#average
+"""courseworkmarks = [34.4, 45, 23.9, 10.2]
+
+totalnumberofelements = len(courseworkmarks)
+
+for x in courseworkmarks:
+    x = sum(courseworkmarks)
+    print(x)
+    
+average = x / totalnumberofelements
+print('Average :', average)"""
+
+
+#for loop to find the largest numberin a list of integers
+"""list = [15, 42, 73, 29, 91, 50]"""
+
+"""largestnumber = list[0]
+for i in list:
+    if i > largestnumber: 
+        largestnumber = i
+        
+        
+print(f"the largest number is {largestnumber}")"""
+
+#print the middle ones
+"""print(len(list[1:4]))
+
+
+
+#while loop to filter out and print even numbers
+list = [12, 17, 24, 29, 36, 43, 50]
+
+#initialize index sice we have a list 
+i = 0
+length = len(list)
+#while loop to go through the list
+
+while i < length:
+    #if the current number is even, print it
+    if list[i] % 2 == 0:
+        print(list[i])
+    i = i + 1"""
+
+
+
+#while
+"""district_list = ["kabale","tororo"]
+condition = 0
+while condition < len(district_list):
+    print(district_list[condition])
+    condition = condition + 1 """
+    
+    
+numbers = [1,2,3,4,5,6,7,8]
+i = 0
+while i <= len(numbers):
+    if i %  2 != 0:
+     print(numbers[i])
+    i = i +1 
+        
+
+
+
+
+
+
+        
+
+
+
