ac2bc866810e0769205b70faac7893fbcb3dddc0
shantal hilda <shanhildah2@gmail.com>
2025-01-09 22:57:40 +0300

    Update 13. IntersectionOfwoArrays.js

--- 13. IntersectionOfwoArrays.js
+++ 13. IntersectionOfwoArrays.js
@@ -3,5 +3,5 @@ function intersection(arr1, arr2) {
 }
 
 let array1 = [1, 2, 3, 4, 5];
-let array2 = [4, 5, 6, 7, 8];
+let array2 = [4, 5, 6, 7];
 console.log(intersection(array1, array2)); // Output: [4, 5]
