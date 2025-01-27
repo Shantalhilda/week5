2ab233486d079bb66e9424c7b34a7a9f4dbe13c8
shantal hilda <shanhildah2@gmail.com>
2025-01-27 10:14:29 +0300

    Update 1. CapitalizeFirstLetter.js

--- 1. CapitalizeFirstLetter.js
+++ 1. CapitalizeFirstLetter.js
@@ -2,4 +2,4 @@ function capitalizeWords(string) {
     return stribg.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
 
-console.log(capitalizeWords("hello world")); // Output: "Hello World"
+console.log(capitalizeWords("hello uganda")); // Output: "Hello uganda"
