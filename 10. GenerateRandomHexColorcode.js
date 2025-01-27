671f0867b0b135e8c20140eac88345c8cd3b1676
shantal hilda <shanhildah2@gmail.com>
2025-01-27 10:16:02 +0300

    Update 10. GenerateRandomHexColorcode.js

--- 10. GenerateRandomHexColorcode.js
+++ 10. GenerateRandomHexColorcode.js
@@ -2,4 +2,4 @@ function getRandomHexColor() {
     return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
 }
 
-console.log(getRandomHexColor()); // Output: Random hex color code like "#a1b2c3"
+console.log(getRandomHexColor()); // Output: Random hex color code like "#a1c2c3"
